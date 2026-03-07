e5a06f1fdbe0958c8d912bbe259c4bf6b2cf63ae
Azhar Hanin <azharhanin002@gmail.com>
2026-03-07 16:32:21 +0700

    fix database

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,20 +1,23 @@
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-  var prisma: PrismaClient | undefined;
-}
-
-export const prisma =
-  global.prisma ??
-  new PrismaClient({
-    log:
-      process.env.NODE_ENV === "development"
-        ? ["query", "error", "warn"]
-        : ["error"],
+/**
+ * Mencegah instansi PrismaClient ganda saat hot-reloading di development.
+ */
+const prismaClientSingleton = () => {
+  return new PrismaClient({
+    log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
   });
+};
 
-if (process.env.NODE_ENV !== "production") {
-  global.prisma = prisma;
+declare global {
+  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
 }
+
+// Gunakan instansi yang sudah ada atau buat baru jika belum tersedia
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
 export default prisma;
+
+if (process.env.NODE_ENV !== "production") {
+  globalThis.prismaGlobal = prisma;
+}
